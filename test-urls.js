423220bb9e409876ec5d81943f10f96347bee086
JinnaLong <58503364+fansanqiu@users.noreply.github.com>
2026-08-31 19:40:16 +0800

    style: replace settings icon with updated SVG and apply flexible sizing to button icons

--- test-urls.js
+++ test-urls.js
@@ -3,4 +3,6 @@ module.exports = [
   'https://www.youtube.com/watch?v=aqz-KE-bpKQ',
   'https://x.com/i/status/2093270659824529461',
   'https://www.pinterest.com/pin/8162843071239973/',
+  'https://uk.pinterest.com/pin/1001558404651339884/',
+  'https://uk.pinterest.com/pin/8162843071239973/',
 ];
